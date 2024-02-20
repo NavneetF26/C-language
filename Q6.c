10f2f3ae8cab0c47f821b92a7a85f27ea0d4d262
NavneetF26 <158492313+NavneetF26@users.noreply.github.com>
2024-02-20 22:18:00 +0530

    Update Q6.c

--- Q6.c
+++ Q6.c
@@ -1,3 +1,4 @@
+//* To input a 2D array of size m*n and perform mirror reversed operation vertically *//
 #include <stdio.h>
 int main()
 {
@@ -33,3 +34,89 @@ int main()
  return 0;
 }
 
+//* To input a 2D array of size m*n and perform mirror reversed operation horizontally *//
+#include <stdio.h>
+int main()
+{
+ int a[10][10],i,j,t,m,n,k;
+ printf("Input no of rows & columns\n"); 
+ scanf("%d%d",&m,&n); 
+ printf("Input array elements\n");
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   scanf("%d",&a[i][j]);
+  }
+ }
+ for(j=0;j<n;j++)
+ {
+  for(i=0,k=m-1;i<n/2;i++,k--)
+  {
+   t=a[i][j];
+   a[i][j]=a[k][j];
+   a[k][j]=t;
+  }
+ }
+ printf("Reverse Mirror Operation Horizontally is:\n"); 
+ for (i=0;i<m;i++)
+ {
+  for(j=0;j<n; j++)
+  {
+   printf ("%d\t",a[i][j]);
+  }
+  printf("\n");
+ }
+ return 0;
+}
+
+//* To input a 2D array of size m*n and perform mirror reversed operation across principle diagonal *//
+#include <stdio.h>
+int main()
+{
+    int a[10][10],i,j,m,n,k,temp;
+    printf("Input no of rows and columns in a square matrix\n");
+    scanf("%d%d",&m,&n);
+    if(m!=n)
+    {
+        printf("Operation is not possible\n");
+    }
+    else
+    {
+     printf("Input array elments\n");
+     for(i=0;i<m;i++)
+     {
+         for(j=0;j<n;j++)
+         {
+             scanf("%d",&a[i][j]);
+         }
+     }
+     k=m-1;
+     int c=1;
+     for(i=0;i<m;i++)
+     {
+         for(j=0;j<n;j++)
+         {
+            if(i+j<=m/2)
+            {
+             temp=a[i][j];
+             a[i][j]=a[i+k][j+k];
+             a[i+k][j+k]=temp;
+            }
+            k--;
+         }
+      k=m-1-c;
+      c++;
+     }
+     printf("Reverse Mirror Operation across principle diagonal is:\n");
+     for(i=0;i<m;i++)
+     {
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+     }
+    }
+    return 0;
+}
